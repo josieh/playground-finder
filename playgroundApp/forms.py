8c32dd5c45dfc934ab6c5f9603b3ef9588be9e98
josieh <hollingsworth.josie@gmail.com>
2014-04-13 23:08:25 -0400

    new styles

--- playgroundApp/forms.py
+++ playgroundApp/forms.py
@@ -2,6 +2,5 @@ from django import forms
 class login (forms.Form):
        name=forms.CharField(max_length=50)
 
-class suggestForm (forms.Form):
+class addReviewForm (forms.Form):
       name = forms.CharField(max_length=50)
-      
