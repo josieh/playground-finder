461dd1df34a8c993096f8b29c42d685655897ccb
katiemarriner <kemarriner@gmail.com>
2014-04-19 19:25:08 -0400

    working with jquery and dropdown not successful

--- playgroundApp/views.py
+++ playgroundApp/views.py
@@ -14,7 +14,6 @@ import json
 def testCreate(request):
 	if request.POST:
 		form = suggestTest(request.POST)
-		#I think this is where we would set the field for latLon
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect(reverse('playgroundapp_home'))
@@ -24,7 +23,6 @@ def testCreate(request):
 	args.update(csrf(request))
 	
 	args['form'] = form
-	args['dropdown'] = SchoolDistrict.objects.values('districtname').distinct()
 	return render_to_response('playgroundapp/create_playground.html', args)
 
 def Playground_List(request):
@@ -90,28 +88,28 @@ def playgroundGeoCodes(request):
 
 def playgroundDetail (request, pk):
 	playground = get_object_or_404(Playground, id=pk)
-	'''
+	
 	features = Features.objects.all()
 	features = features.filter(playgroundID=pk)
 	features = features[0]
-	'''
+	
 	
 	schoolDistrict = SchoolDistrict.objects.all()
 	schoolDistrict = schoolDistrict.filter(schoolDistrictID = playground.schoolDistrictID)
 	schoolDistrict = schoolDistrict[0]
-	'''
+	
 	ages = Age.objects.all()
 	ages = ages.filter(ageID = playground.ageID)
 	ages = ages[0]
 
 	transport = TransportationFeatures.objects.all().filter(playgroundID = playground.playgroundID)[0]
-	'''
+
 	context = {
 		'playground':playground,
 		'schoolDistrict':schoolDistrict,
-		#'features':features,
-		#'age':ages,
-		#'transport':transport,
+		'features':features,
+		'age':ages,
+		'transport':transport,
 	}
 	return render (request, "playgroundApp/playground_info.html", context)
 
